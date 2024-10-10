d03b61e9619f40b86cf5c3b4ea4499f681231c9f
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-10 01:44:29 -0300

    feat: started the barbershop project with Spring

---
+++ src/main/java/com/barbershop/barbershop_backend/BarbershopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.barbershop.barbershop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbershopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barbershop/barbershop_backend/BarbershopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbershop.barbershop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
