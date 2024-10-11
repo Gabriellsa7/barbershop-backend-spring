132584924e6beebe1cf90e48890180c5ae7ed8c4
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-11 14:44:58 -0300

    feat: added @Column notation in some attributes

--- src/main/java/com/barbershop/barbershop_backend/model/Client.java
+++ src/main/java/com/barbershop/barbershop_backend/model/Client.java
@@ -20,13 +20,16 @@ public class Client {
     @UuidGenerator
     private UUID id;
 
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(unique = true, nullable = false)
+    @Column(name = "email", unique = true, nullable = false)
     private String email;
 
+    @Column(name = "password", nullable = false)
     private String password;
 
+    @Column(name = "phone")
     private String phone;
 
     @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
