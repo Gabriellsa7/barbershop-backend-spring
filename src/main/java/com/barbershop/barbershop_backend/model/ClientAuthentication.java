39bb8d428a48e36ac7243e9588e7319aa3d1bf23
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-11 01:37:14 -0300

    feat: created ClientAuthentication model

---
+++ src/main/java/com/barbershop/barbershop_backend/model/ClientAuthentication.java
@@ -0,0 +1,29 @@
+package com.barbershop.barbershop_backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "client_authentication")
+// Data Lombok add setters and getters
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientAuthentication {
+
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    private String token;
+
+    @OneToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+}
