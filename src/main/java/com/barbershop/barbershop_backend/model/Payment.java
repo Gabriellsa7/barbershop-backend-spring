e7eef646940d4efb24c125e702ff35104fbc0845
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-12 00:52:58 -0300

    feat: created the payment model

---
+++ src/main/java/com/barbershop/barbershop_backend/model/Payment.java
@@ -0,0 +1,40 @@
+package com.barbershop.barbershop_backend.model;
+
+import com.barbershop.barbershop_backend.enums.PaymentMethod;
+import com.barbershop.barbershop_backend.enums.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "payment")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    @Column(name = "value", nullable = false)
+    private Float value;
+
+    @Column(name = "payment_method", nullable = false)
+    private PaymentMethod paymentMethod;
+
+    @Column(name = "status", nullable = false)
+    private Status status;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false, referencedColumnName = "id")
+    private Client client;
+
+    @OneToOne
+    @JoinColumn(name = "haircut_reservation_id", nullable = false, referencedColumnName = "id")
+    private  HairCutReservation hairCutReservation;
+}
