28c9f252aab60a7ccbf895c0c8a456510aa034dd
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-12 00:52:50 -0300

    feat: created the haircut model

---
+++ src/main/java/com/barbershop/barbershop_backend/model/HairCut.java
@@ -0,0 +1,38 @@
+package com.barbershop.barbershop_backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "haircut")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HairCut {
+
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "barbershop_id", referencedColumnName = "id")
+    private BarberShop barberShop;
+
+    @OneToOne
+    @JoinColumn(name = "hair_service_id", referencedColumnName = "id")
+    private HairService hairService;
+}
