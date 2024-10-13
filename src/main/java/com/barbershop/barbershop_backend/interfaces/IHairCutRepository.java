dfa20191207b7a70b2db7f089a141238bf5582d5
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-13 00:17:00 -0300

    feat: created the IHairCutRepository

---
+++ src/main/java/com/barbershop/barbershop_backend/interfaces/IHairCutRepository.java
@@ -0,0 +1,18 @@
+package com.barbershop.barbershop_backend.interfaces;
+
+import com.barbershop.barbershop_backend.model.HairCut;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IHairCutRepository {
+    HairCut createHairCut(HairCut hairCut);
+
+    List<HairCut> getHairCut();
+
+    Optional<HairCut> getHairCutById(String id);
+
+    HairCut updateHairCut(String id, HairCut updateHairCut);
+
+    void deleteHairCut(String id);
+}
