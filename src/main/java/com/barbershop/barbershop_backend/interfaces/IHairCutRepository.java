c2c563f77adf4521f7c642ca845c2908af475aa3
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-14 04:27:13 -0300

    feat: created hair cut repository interface

--- src/main/java/com/barbershop/barbershop_backend/interfaces/IHairCutRepository.java
+++ src/main/java/com/barbershop/barbershop_backend/interfaces/IHairCutRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 public interface IHairCutRepository {
     HairCut createHairCut(HairCut hairCut);
 
-    List<HairCut> getHairCut();
+    List<HairCut> getHairCuts();
 
     Optional<HairCut> getHairCutById(String id);
 
