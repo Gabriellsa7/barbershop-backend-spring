5479d6b925f9d20240bdbfeef5b3a5dc88ea2468
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-16 12:43:35 -0300

    feat: begin the HairServiceRepository

--- src/main/java/com/barbershop/barbershop_backend/repository/HairServiceRepository.java
+++ src/main/java/com/barbershop/barbershop_backend/repository/HairServiceRepository.java
@@ -1,4 +1,13 @@
 package com.barbershop.barbershop_backend.repository;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@Entity
+@Transactional
 public class HairServiceRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
 }
