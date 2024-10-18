9c336c22882b1bd139036290b8d10754186cf9e0
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-18 15:35:44 -0300

    feat: changed the id type in client class

--- src/main/java/com/barbershop/barbershop_backend/model/Client.java
+++ src/main/java/com/barbershop/barbershop_backend/model/Client.java
@@ -17,7 +17,7 @@ import java.util.UUID;
 public class Client {
 
     @Id
-    @UuidGenerator
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
     @Column(name = "name", nullable = false)
