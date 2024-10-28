f73994f963501964c283152f1d7f7f84212b8e8a
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-28 09:37:46 -0300

    feat: changed the id type

--- src/main/java/com/barbershop/barbershop_backend/interfaces/IHairCutRepository.java
+++ src/main/java/com/barbershop/barbershop_backend/interfaces/IHairCutRepository.java
@@ -4,15 +4,16 @@ import com.barbershop.barbershop_backend.model.HairCut;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface IHairCutRepository {
     HairCut createHairCut(HairCut hairCut);
 
     List<HairCut> getHairCuts();
 
-    Optional<HairCut> getHairCutById(String id);
+    Optional<HairCut> getHairCutById(UUID id);
 
-    HairCut updateHairCut(String id, HairCut updateHairCut);
+    HairCut updateHairCut(UUID id, HairCut updateHairCut);
 
-    void deleteHairCut(String id);
+    void deleteHairCut(UUID id);
 }
