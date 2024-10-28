e826c3a4c5a74702fbf5643568e4fbed7ddc147e
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-28 09:37:51 -0300

    feat: changed the id type

--- src/main/java/com/barbershop/barbershop_backend/interfaces/IHairCutReservationRepository.java
+++ src/main/java/com/barbershop/barbershop_backend/interfaces/IHairCutReservationRepository.java
@@ -4,22 +4,23 @@ import com.barbershop.barbershop_backend.model.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface IHairCutReservationRepository {
     HairCutReservation createReservation(HairCutReservation reservation);
 
     List<HairCutReservation> getReservations();
 
-    Optional<HairCutReservation> getReservationById(String id);
+    Optional<HairCutReservation> getReservationById(UUID id);
 
-    HairCutReservation updateReservation(String id, HairCutReservation updateReservation);
+    HairCutReservation updateReservation(UUID id, HairCutReservation updateReservation);
 
-    void deleteReservation (String id);
+    void deleteReservation (UUID id);
 
     //method to get a barbershop ID, used in a validation
-    Optional<BarberShop> getBarberShopId(String barberShop_id);
+    Optional<BarberShop> getBarberShopId(UUID barberShop_id);
     //method to get a hair service ID, used in a validation
-    Optional<HairService> getHairServiceId(String hairService_id);
+    Optional<HairService> getHairServiceId(UUID hairService_id);
     //method to get a client ID, used in a validation
-    Optional<Client> getClientId(String client_id);
+    Optional<Client> getClientId(UUID client_id);
 }
