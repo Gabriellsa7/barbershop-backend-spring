96b46dca034cae81b80cb164fc22de0a606408cc
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-28 09:28:25 -0300

    feat: created haircut repository

--- src/main/java/com/barbershop/barbershop_backend/repository/HairCutRepository.java
+++ src/main/java/com/barbershop/barbershop_backend/repository/HairCutRepository.java
@@ -1,19 +1,20 @@
 package com.barbershop.barbershop_backend.repository;
 
 import com.barbershop.barbershop_backend.interfaces.IHairCutRepository;
-import com.barbershop.barbershop_backend.model.Client;
 import com.barbershop.barbershop_backend.model.HairCut;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 @Transactional
-public class HairCurRepository implements IHairCutRepository {
+public class HairCutRepository implements IHairCutRepository {
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -29,19 +30,20 @@ public class HairCurRepository implements IHairCutRepository {
     }
 
     @Override
+    @Query("SELECT c FROM HairCut c")
     public List<HairCut> getHairCuts() {
-        return  entityManager.createQuery("SELECT u FROM client u", HairCut.class).getResultList();
+        return  entityManager.createQuery("SELECT c FROM HairCut c", HairCut.class).getResultList();
     }
 
     @Override
-    public Optional<HairCut> getHairCutById(String id) {
+    public Optional<HairCut> getHairCutById(UUID id) {
         HairCut hairCut = entityManager.find(HairCut.class, id);
 
         return Optional.ofNullable(hairCut);
     }
 
      @Override
-    public HairCut updateHairCut(String id, HairCut updateHairCut) {
+    public HairCut updateHairCut(UUID id, HairCut updateHairCut) {
          HairCut hairCut = entityManager.find(HairCut.class, id);
          if (hairCut != null) {
             hairCut.setName(updateHairCut.getName());
@@ -56,7 +58,7 @@ public class HairCurRepository implements IHairCutRepository {
     }
 
     @Override
-    public void deleteHairCut(String id){
+    public void deleteHairCut(UUID id){
         HairCut hairCut = entityManager.find(HairCut.class, id);
 
         if(hairCut != null) {
