657a997508899ea8334bf56a1461815b4524c035
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-11-17 02:19:57 -0300

    feat: created and configured the AuthController

---
+++ src/main/java/com/barbershop/barbershop_backend/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.barbershop.barbershop_backend.controller;
+
+import com.barbershop.barbershop_backend.infra.security.JwtUtil;
+import com.barbershop.barbershop_backend.model.Client;
+import com.barbershop.barbershop_backend.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody Client loginRequest) {
+        Optional<Client> clientOptional = clientService.getClientByEmail(loginRequest.getEmail());
+
+        if (clientOptional.isPresent()) {
+            Client client = clientOptional.get();
+            // Verifica se a senha do banco coincide com a senha fornecida
+            if (client.getPassword().equals(loginRequest.getPassword())) {
+                String token = jwtUtil.generateToken(client.getEmail());
+                return ResponseEntity.ok(token);
+            } else {
+                return ResponseEntity.status(401).body("Invalid password");
+            }
+        }
+        return ResponseEntity.status(401).body("User not found");
+    }
+}
