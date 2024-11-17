068122e9d3c21295a71f1d05ac5c8d506b484074
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-11-17 02:20:24 -0300

    feat: created and configured the SecurityConfig

---
+++ src/main/java/com/barbershop/barbershop_backend/infra/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.barbershop.barbershop_backend.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Desabilita CSRF (opcional, dependendo do uso)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/clients", "/auth/login").permitAll() // Permite acesso público ao cadastro e login
+                        .anyRequest().authenticated() // Exige autenticação para outros endpoints
+                );
+        return http.build();
+    }
+}
